55f84acc065081d337d00f0b4507fae9dd606ece
fusillicode <fusillicode@gmail.com>
2016-10-02 09:38:17 +0200

    random fixes to suppress elixir warnings

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,5 @@
 defmodule Metr0.PageController do
   use Metr0.Web, :controller
-  alias Metr0.Repo
 
   def index(conn, _params, current_user, _claims) do
     render conn, "index.html", current_user: current_user
